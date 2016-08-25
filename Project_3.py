d9899154a05622dda5fbfa73924c689e54aaefb6
jinalee2531 <jinalee@andrew.cmu.edu>
2016-08-25 18:18:31 -0400

    r2.1

--- Project_3.py
+++ Project_3.py
@@ -5,66 +5,34 @@
 # 
 # 
 # This project extracted and cleaned OpenStreetMap data(https://en.wikipedia.org/wiki/OpenStreetMap) and stored it into MongoDB so that users can explore quality data fast.
-# 
-# OpenStreetMap is map information in a topological data structure collected by volunteers. It consists of four major elemnts; Node, Ways, Relation and Tags. Nodes present geographic information of specipied locations by points and create log such as user, update time etc. Ways represent linear features such as areas, rivers, streets and create log such as user, update time etc. Relations are representing relationship between Nodes and Ways, and Tags contains metadata of its Nodes or Ways elements.
-# 
-# I used OpenSteetMap New York area of OSM filetype downloaded from MapZen (https://mapzen.com/data/metro-extracts/). Its size is about 737,545kb and it contains 3,449,666 elements consists of 2,961,075 'node's, 2571 'relation's, and 486020 'way' tags.
-# 
-# Assessing and improving data quality, this project cosidered validity, accuracy, completness, consistency, and uniformity of data. The whole process of project are as below:
+# OpenStreetMap is map information in a topological data structure collected by volunteers. It consists of four major elements; Node, Ways, Relation and Tags. Nodes present geographic information of specified locations by points and create log such as user, update time etc. Ways represent linear features such as areas, rivers, streets and create log such as user, update time etc. Relations are representing relationship between Nodes and Ways, and Tags contains metadata of its Nodes or Ways elements.
+# I used OpenStreetMap New York area of OSM file downloaded from MapZen (https://mapzen.com/data/metro-extracts/). Its size is about 737,545kb and it contains 3,449,666 elements consists of 2,961,075 'node's, 2571 'relation's, and 486020 'way' tags.
+# Assessing and improving data quality, this project considered validity, accuracy, completeness, consistency, and uniformity of data. The whole process of project are as below:
 # 
 # 
-# 
-# ## 1 Auditing Data
-# First, this project audited the data's quality in five perspectives to assure concrete data analysis result.
-#      * Completness
-#        : Computing descriptive statistics of data
-#          - Number of elements
-#          - Counted number of records for each attribute and missing values.     
-#      * Validity
-#        : Audited if street name, postcode, height data are in appreciate formats.         
-#      * Accuracy
-#        : Checked if postcode starts with New York's code: 0 or 1.     
-#      * Consistency
-#        : Not applicable here since, this project use data from only one source.     
-#      * Uniformity
-#        : Checked 'height' if it is in the same unit, and corrected data in different units to foot.
-#  
-# ## 2 Reshapenig Data
-# Designed the shape of data, reflecting attribute types revealed through auditing.
-#      * Filtering out attributes that have non-character name.     
-#      * Grouped related attributes
-#         - created: to data creation information in a dictionary such as user, timestamp, etc.
-#         - address: grouped attributes under 'Address' such as addr:housenumber, addr:zipcode etc.
-#         - others which has sub-categories : grouped attributes that its names contains ':', for example, 'gins:housenumber.'     
-#      * Combining 'latitude' value and 'longtitude' value into a list 'pos' for using GEO2D index in MongoDB     
-#      * Transformed data type of timestamp from string to datetime object.
-#      
-# ## 3 Imported Data to MongoDB 
-# Transformed in JSON File and created indexes of fields which are used for queries later.
-#     
-# ## 4 Run Queries
-# Runed queries to answer these questions.
-#     * Question 1: Top 10 users who uploaded the data most often and frequency
-#     * Question 2: The number of data uploaded by year.
-#     * Question 3: Finding the five nearest locations from "AJ's Pizza".
-#     
-# ## 5 Conclusion
-# This project focused on improving data of major attributes, which are occupying more than 80% of data, and worked well enough to answer arisen questions in the project.
-# However, there are still hundreds of field not cleaned yet. Especially, attributes that have sub categories such as gnis, tiger, cityrack seemed to have overlapping with other attributes. If we are given more time and enough information about these categories, we might try to improve the quality of data by cross-checking values of each attribute and integrating related attributes as many as possible.
 
-# In[1]:
 
 ### Auditing
 
 import xml.etree.ElementTree as ET
 import os
 import re
-from collections import defaultdict
+from  collections import defaultdict
 import pprint
 
+from datetime import datetime
 
-# updating/initiating value for each attributes in the data
+import json
+from bson import json_util
+
+
+# In[2]:
+
+
+
 def add_to_dict(dic, dic_key, key_to_cnt):
+    """Updating/initiating value for each attributes in the data."""
+    
     updated_dic = dic
     try    : updated_dic[dic_key][key_to_cnt] +=1
     except : updated_dic[dic_key][key_to_cnt] = 1
@@ -133,7 +101,6 @@ def stats_data(osm_file_name_input, pretty = False, tags = ('node', 'way', 'rela
             ## collecting keys and its values if the key is containing punctuations
             if key.startswith('cityracks'):
                 key = ":".join(key.split('.'))
-                #print key
             
             if problemchars.search(key) :
                 try:
@@ -150,7 +117,7 @@ def stats_data(osm_file_name_input, pretty = False, tags = ('node', 'way', 'rela
                  
                 if group_key not in element_dict.keys():
                     element_dict[group_key]= defaultdict(dict) 
-                #print group_key, sub_group_key, val
+                
                 try:
                     element_dict[group_key][sub_group_key]=val
                 except :
@@ -166,41 +133,48 @@ def stats_data(osm_file_name_input, pretty = False, tags = ('node', 'way', 'rela
                     sub[sub_group_key] +=1
                 except KeyError:
                     sub[sub_group_key] =1
-                #print '3: ',element_dict
+
             else :
                 element_dict[key] = val                
                 stats = add_to_dict(dic = stats, dic_key='tag_attrib_count', key_to_cnt= key)
-            #print '4: ',element_dict
+            
         
         data[id_] = element_dict
         
         root.clear()
-        #print '5: ',element_dict
+
         
     return data, stats, errors
 
 
-# In[2]:
-
-import os
+# In[3]:
 
 OSM_FILE_NAME = 'new_new-york_new-york'
 OSM_FILE = os.path.join('data',OSM_FILE_NAME+".osm")
-SAMPLE_FILE_NAME ='sample_midnew-york_new-york' 
+SAMPLE_FILE_NAME ='mini_sample_new-york_new-york' 
 SAMPLE_FILE =os.path.join('data',SAMPLE_FILE_NAME+".osm")
-
-osm_file_name =OSM_FILE_NAME
-osm_file= OSM_FILE
-
-
-# In[3]:
-
-data, stats, errors  = stats_data(osm_file)
 
 
 # In[4]:
 
-import pprint
+if False:
+    osm_file_name =OSM_FILE_NAME
+    osm_file= OSM_FILE
+
+
+# In[5]:
+
+if True:
+    osm_file_name =SAMPLE_FILE_NAME
+    osm_file= SAMPLE_FILE
+
+
+# In[6]:
+
+data, stats, errors  = stats_data(osm_file)
+
+
+# In[7]:
 
 pprint.pprint(stats)
 
@@ -214,22 +188,14 @@ print tag_attrib_to_exclude
 
 # In[9]:
 
-from datetime import datetime
-import re
-
+"""
 problemchars = re.compile(r'[=\+/&<>;\'"\?%#$@\,\. \t\r\n]')
 
 CREATED = [ "version", "changeset", "timestamp", "user", "uid"]
 #added 'created_by' attribute of low level elements
 CREATED.append('created_by')
 
-
-### Reshaping Element
-
-from collections import defaultdict
-
-import re
-
+"""
 ### street name : street type in expected
 expected = ["Street", "Avenue", "Boulevard", "Drive", "Court", "Place", "Square", "Lane", "Road", 'Turnpike',
             "Trail", "Parkway", "Commons",'Way','Bayside', 'Circle', 'Highway', 'West', 'South','East', 'North'
@@ -245,8 +211,10 @@ postcode_type_re = re.compile('[0-9]{5}(?:-[0-9]{4})?$')
 
 # In[10]:
 
-### Recording inappropriate date type in 'caution' dictionary and transforming timestamp in datetime object
 def audit_timestamp(str_time):                              
+    """
+    Recording inappropriate date type in 'caution' dictionary and transforming timestamp in datetime object
+    """
     try:
         time = datetime.strptime(str_time, "%Y-%m-%dT%H:%M:%SZ")
     except:
@@ -254,8 +222,11 @@ def audit_timestamp(str_time):
         time=str_time
     return time
 
-### Recording inappropriate street_type in 'caution' dictionary and returns it corrected.
 def audit_street(street_name, id_):
+    """
+    Recording inappropriate street_type in 'caution' dictionary and returns it corrected.
+    """
+    
     street_type_re = re.compile(r'\S+\.?$', re.IGNORECASE)
     try: 
         # removing space start and end of the string and capitalize it.
@@ -283,8 +254,11 @@ def audit_street(street_name, id_):
     
     return to_return
 
-### Recording inappropriate postcode data in 'caution' dictionary and returns it corrected.
+
 def audit_postcode(postcode, id_):
+    """
+    Recording inappropriate postcode data in 'caution' dictionary and returns it corrected.
+    """
     
     postcode = postcode.strip()
     p = postcode_type_re.search(postcode)
@@ -295,8 +269,14 @@ def audit_postcode(postcode, id_):
         to_return=postcode
     return to_return
 
-### Recording inappropriate position data in 'caution' dictionary and returns it in a list of float.
+
 def audit_pos(lat,lon):
+    """
+    Recording inappropriate position data in 'caution' dictionary and returns it in a list of float.
+    
+    The valid range of latitude in degrees is -90 and +90 for the southern and northern hemisphere respectively.
+        Longitude is in the range -180 and +180 specifying the east-west position.
+    """
 
     
     try:
@@ -305,11 +285,7 @@ def audit_pos(lat,lon):
                 
         lat = float(lat)
         lon = float(lon)
-        
-        """
-        The valid range of latitude in degrees is -90 and +90 for the southern and northern hemisphere respectively.
-        Longitude is in the range -180 and +180 specifying the east-west position.
-        """
+
         if not -90<=lat<=90 or not -180<=lon<=180:
             cautions['pos'].append([lat, lon])
 
@@ -318,8 +294,11 @@ def audit_pos(lat,lon):
     
     return [lat,lon]
 
-### Recording inappropriate height data in 'caution' dictionary and returns it in float and in feet.
+
 def audit_height(height,id_):
+    """
+    Recording inappropriate height data in 'caution' dictionary and returns it in float and in feet.
+    """
     
     if isinstance(height, float):
         return height
@@ -327,11 +306,11 @@ def audit_height(height,id_):
         # remove foot symbol(') from the data
         if height.endswith("'"):
             height = float(height.replace("'",""))
-            #cautions['height'][id]= element['height']
+            
         # remove inch symbol("") from the data
         elif height.endswith('"'):
             height = float(height.replace('"',''))/12
-            #cautions['height'][id]=element['height']
+            
         height = float(height)
 
         if height<=4 and element['tag_type']=='node':
@@ -345,9 +324,12 @@ def audit_height(height,id_):
 
 # In[11]:
 
-### method for executing audit data of five attributes of element: street, postcode, timestamp, latitude, longtitude, and height 
-# for an input element.
+
 def auditing_data(element):
+    """
+    Execute audit data of five attributes of element: street, postcode, timestamp, latitude, longtitude, and height 
+    for an input element.
+    """
     
     try:     id_ = element['id']
     except : cautions['id'].append(element)
@@ -495,9 +477,10 @@ print h
 
 # In[19]:
 
-#>> modifying 'audit_height' method to add regex for reshaping the height attribute in the same unit(feet).
-
 def audit_height(height,id_):
+    """
+    Modify 'audit_height' method to add regex for reshaping the height attribute in the same unit(feet).
+    """
 
     inch = ['"', 'inch']
     feet = ['ft', "'", 'feet']
@@ -549,22 +532,15 @@ cautions['pos']
 # 
 # Here, I reshaped the data and saved the data in JSON format to import it to the MongoDB.
 
-# In[21]:
+# In[ ]:
 
-import json
-import pprint
-from  collections import defaultdict
+
 # to save datetime object in json: referred> https://stackoverflow.com/questions/11875770/how-to-overcome-datetime-datetime-not-json-serializable-in-python
-from bson import json_util
+
 
 ### processing data in an input osm file and save it in both JSON format and a list( 'data')
 
 json_file_name_out = "%s.json" %(osm_file_name)
-problemchars = re.compile(r'[=\+/&<>;\'"\?%#$@\,\. \t\r\n]')
-CREATED = [ "version", "changeset", "timestamp", "user", "uid"]
-#added 'created_by' attribute of low level elements
-CREATED.append('created_by')
-
 
 cautions = dict(street=defaultdict(list)
                , postcode=[]
@@ -573,53 +549,63 @@ cautions = dict(street=defaultdict(list)
                , pos=[]
                , height={}) 
 
-with open(json_file_name_out,'w') as output:
-    i=0
-    for _id, element in data.items():
-        ele = {'created':{}}
-        
-        for key, val in element.items():
-            
-            if key in tag_attrib_to_exclude or val == None:
-                continue
-            
-            if key== 'addr':
-                try:
-                    addr = ele[key]
-                except KeyError:
-                    ele[key]={}
-                    
-                for k, v in element[key].items():
-                    if v=={} or v==None:
-                        continue
-                    
-                    if k =='postcode':
-                        ele[key][k] = audit_postcode(v,_id)
-                        
-                    elif k == 'street':
-                        if v!='':
-                            ele[key][k] = audit_street(v,_id)
-                    else :
-                        ele[key][k]=v              
+def osm_to_json(data, json_file_name_out):
+    """
+    Refine data from an original osm file and save it in both JSON format and a list( 'data')
+    
+    """
+    problemchars = re.compile(r'[=\+/&<>;\'"\?%#$@\,\. \t\r\n]')
+    CREATED = [ "version", "changeset", "timestamp", "user", "uid", 'created_by']
+    
+    with open(json_file_name_out,'w') as output:
+        i=0
+        for _id, element in data.items():
+            ele = {'created':{}}
 
-            elif key == 'lat':
-                ele['pos'] = audit_pos(val, element['lon'])
-            elif key =='height':
-                ele[key] = audit_height(val,_id)
-            elif key in CREATED:
-                if key =='timestamp': val = audit_timestamp(val)
-                ele['created'][key]=val
-            else:
-                ele[key] = val
-        
-        ### save the element dictionary in a JSON file
-        output.write(json.dumps(ele, default = json_util.default)+'\n')
-        
-        i+=1
-        if i%100000==1: print i
+            for key, val in element.items():
+
+                if key in tag_attrib_to_exclude or val == None:
+                    continue
+
+                if key== 'addr':
+                    try:
+                        addr = ele[key]
+                    except KeyError:
+                        ele[key]={}
+
+                    for k, v in element[key].items():
+                        if v=={} or v==None:
+                            continue
+
+                        if k =='postcode':
+                            ele[key][k] = audit_postcode(v,_id)
+
+                        elif k == 'street':
+                            if v!='':
+                                ele[key][k] = audit_street(v,_id)
+                        else :
+                            ele[key][k]=v              
+
+                elif key == 'lat':
+                    ele['pos'] = audit_pos(val, element['lon'])
+                elif key =='height':
+                    ele[key] = audit_height(val,_id)
+                elif key in CREATED:
+                    if key =='timestamp': val = audit_timestamp(val)
+                    ele['created'][key]=val
+                else:
+                    ele[key] = val
+
+            ### save the element dictionary in a JSON file
+            output.write(json.dumps(ele, default = json_util.default)+'\n')
+
+            i+=1
+            if i%1000000==1: print i
 
 
-# In[22]:
+# In[ ]:
+
+osm_to_json(data, json_file_name_out)
 
 ### reviewing suspicious error data: street
 for key, val in cautions['street'].items():
@@ -627,7 +613,7 @@ for key, val in cautions['street'].items():
     pprint.pprint(val)
 
 
-# In[23]:
+# In[ ]:
 
 ### reviewing suspicious error data: others
 print cautions['postcode']
@@ -641,10 +627,10 @@ print cautions['height']
 # 
 # In MongoDB Shell, one could import the data in JSON file created just before, typing command below.
 # 
-# "mongoimport --db p3(db name) --collection new_york(collection name) --file datafile.json"
+# "mongoimport --db p3(:db name) --collection new_york(:collection name) --file datafile.json"
 # 
 
-# In[1]:
+# In[32]:
 
 ### Connecting to my MongoDB db, 'p3'
 
@@ -660,7 +646,7 @@ collection = 'new_york'
 # 
 # Here created indexes for fields which are used for queries to improve speed performance. Especially, for 'pos' field, used GEO2D INDEX to make it possible to find near specified locations.
 
-# In[25]:
+# In[ ]:
 
 # Create a 2d Index: https://docs.mongodb.com/v3.0/tutorial/build-a-2d-index/
 #### in pyMongo    : http://api.mongodb.com/python/1.7/examples/geo.html
@@ -678,12 +664,26 @@ if True:
 
 
 # ## Executing Queries
+
+# ### Query 0.1: number of unique users
 # 
+
+# In[78]:
+
+len(db[collection].find(query).distinct('created.uid'))
+
+
+# ### Query 0.2: number of amenities
+
+# In[89]:
+
+query ={'amenity':{'$ne': None}}
+db[collection].find(query).count()
+
+
 # ### Query 1:  Top 10 users who uploaded the data most often and frequency
 
-# In[26]:
-
-import pprint
+# In[43]:
 
 pipeline = [
     {'$group':{'_id': '$created.user', 'frequency': {'$sum':1} }}
@@ -699,7 +699,7 @@ for e in result:
 
 # ### Query 2:  The number of data uploaded by year.
 
-# In[27]:
+# In[34]:
 
 ### run query
 
@@ -718,9 +718,7 @@ for r in result:
 
 # ### Query 3:  Finding the Five Nearest locations from 'AJ's Pizza'.
 
-# In[15]:
-
-import pprint
+# In[35]:
 
 ## looking for position of "AJ's Pizza".
 name = "AJ's Pizza"
@@ -730,14 +728,14 @@ pipeline ={'$query': {'name':name}
 result = db[collection].find(pipeline)
 
 for r in result:
-    #pprint.pprint(r) #['pos'])
+    
     position = r['pos']
     
 print
 print name,"'s location: ", position
 
 
-# In[12]:
+# In[36]:
 
 ### Query a 2d Index: https://docs.mongodb.com/manual/tutorial/query-a-2d-index/
 
@@ -749,4 +747,9 @@ for doc in result:
     print
     pprint.pprint(doc)
     
+
+
+# In[ ]:
+
+
 
